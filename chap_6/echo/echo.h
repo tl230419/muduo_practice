1a9dc0dbd83aa93d6c11612b7dc008bbfa0c0f6d
Allen <tl230419@163.com>
2021-02-26 01:55:07 -0800

    add muduo echo example

---
+++ chap_6/echo/echo.h
@@ -0,0 +1,25 @@
+#ifndef ECHO_H
+#define ECHO_H
+
+#include <muduo/net/TcpServer.h>
+
+class EchoServer
+{
+public:
+    EchoServer(muduo::net::EventLoop* loop,
+                const muduo::net::InetAddress& listenAddr);
+
+    void start();
+
+private:
+    void onConnection(const muduo::net::TcpConnectionPtr& conn);
+
+    void onMessage(const muduo::net::TcpConnectionPtr& conn,
+                    muduo::net::Buffer* buf,
+                    muduo::Timestamp time);
+
+    muduo::net::EventLoop* loop_;
+    muduo::net::TcpServer server_;
+};
+
+#endif // ECHO_H
